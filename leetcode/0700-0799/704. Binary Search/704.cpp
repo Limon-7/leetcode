fe5d6ca1d25246e1ff390104b0a4191067513e21
limon-7 <liibd7@gmail.com>
2024-06-16 04:03:41 +0600

    Add solution of 704. Binary Search

---
+++ leetcode/0700-0799/704. Binary Search/704.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (nums[mid] > target) {
+                r = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+// TC: O(log n)
---
+++ leetcode/0700-0799/704. Binary Search/704.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + ((r - l) >> 1)
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                return mid
+        return -1
+# TC: O(log n)
