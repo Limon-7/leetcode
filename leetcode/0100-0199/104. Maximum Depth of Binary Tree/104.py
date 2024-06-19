6bd01b95e56da8f479f467c248e1b2e21feafae2
limon-7 <liibd7@gmail.com>
2024-06-19 21:57:23 +0600

    Feat: add solution 104.py using BFS

---
+++ leetcode/0100-0199/104. Maximum Depth of Binary Tree/104.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        queue = [root]
+        l = 0
+        while len(queue) > 0:
+            for _ in range(len(queue)):
+                curr = queue.pop(0)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            l += 1
+        return l
