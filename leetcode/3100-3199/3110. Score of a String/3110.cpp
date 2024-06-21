c005b965e832648b815dbf7cf0cdac23c34d46e7
limon-7 <liibd7@gmail.com>
2024-06-22 01:52:20 +0600

    Feat: add solution 26.Score of a string

---
+++ leetcode/3100-3199/3110. Score of a String/3110.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int res = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            res += abs(int(s[i]) - int(s[i + 1]));
+        }
+        return res;
+    }
+};
