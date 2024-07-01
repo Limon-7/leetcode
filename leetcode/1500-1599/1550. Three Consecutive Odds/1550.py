d6fe3516a44c9d6f7bab490eb6e94a9de1a2799f
limon-7 <liibd7@gmail.com>
2024-07-01 23:45:53 +0600

    Feat: add solution 1550.py

---
+++ leetcode/1500-1599/1550. Three Consecutive Odds/1550.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        cnt = 0
+        for v in arr:
+            if v % 2:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt == 3:
+                return True
+
+        return False
