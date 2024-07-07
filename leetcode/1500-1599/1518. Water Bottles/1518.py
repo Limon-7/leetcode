697f6dbb7c8e78fdfe8312808bd71ab47cba8578
limon-7 <liibd7@gmail.com>
2024-07-07 18:17:25 +0600

    Feat: add solution 1518.py

---
+++ leetcode/1500-1599/1518. Water Bottles/1518.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        res = numBottles
+
+        while numBottles >= numExchange:
+            numBottles = numBottles - numExchange + 1
+            res += 1
+
+        return res
